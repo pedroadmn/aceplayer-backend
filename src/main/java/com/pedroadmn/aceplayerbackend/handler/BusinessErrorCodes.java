335e1c8212c37a38fe6d17f37b3ac79d963ecf33
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2025-01-31 08:01:40 -0300

    add error handler methods and cors config

---
+++ src/main/java/com/pedroadmn/aceplayerbackend/handler/BusinessErrorCodes.java
@@ -0,0 +1,26 @@
+package com.pedroadmn.aceplayerbackend.handler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+public enum BusinessErrorCodes {
+    NO_CODE(0, HttpStatus.NOT_IMPLEMENTED, "No Code"),
+    INCORRECT_CURRENT_PASSWORD(300, HttpStatus.BAD_REQUEST, "Current password is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, HttpStatus.BAD_REQUEST, "The new password does not match"),
+    ACCOUNT_LOCKED(302, HttpStatus.BAD_REQUEST, "User account is locked"),
+    ACCOUNT_DISABLED(303, HttpStatus.FORBIDDEN, "User account is disabled"),
+    BAD_CREDENTIALS(304, HttpStatus.FORBIDDEN, "Login and / or password is incorrect")
+    ;
+    @Getter
+    private final int code;
+    @Getter
+    private final HttpStatus httpStatus;
+    @Getter
+    private final String description;
+
+    BusinessErrorCodes(int code, HttpStatus httpStatus, String description) {
+        this.code = code;
+        this.httpStatus = httpStatus;
+        this.description = description;
+    }
+}
