34cc6c9d3dcf0f7370c50fa4148c3e9960752634
Pedro Mello <pedroadmn@gmail.com>
2025-02-07 19:11:54 -0300

    update dockerfile to deploy the app on render platform

--- src/main/java/com/pedroadmn/aceplayerbackend/infra/security/ApplicationConfig.java
+++ src/main/java/com/pedroadmn/aceplayerbackend/infra/security/ApplicationConfig.java
@@ -46,7 +46,7 @@ public class ApplicationConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://aceplayeroficial.com.br/"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
